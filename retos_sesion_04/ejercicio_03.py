c5cfd0f10d21c7153207773b279aba6cfb64b730
ArielQ1 <quizayaariel@gmail.com>
2024-04-30 08:55:56 -0400

    adicion de ejercicio_03 sesion 4

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,26 @@
+print("Bienvenido al juego de Ajedrez\n")
+
+print("Reglas del Ajedrez:")
+print("- El ajedrez es un juego de estrategia entre dos jugadores en un tablero de 8x8 casillas.")
+print("- Cada jugador tiene un conjunto de 16 piezas al comienzo del juego: 1 rey, 1 reina, 2 torres, 2 caballos, 2 alfiles y 8 peones.")
+print("- El objetivo del juego es dar jaque mate al rey del oponente, lo que significa que el rey está amenazado y no tiene movimientos legales para escapar.")
+
+print("\nMovimiento de las piezas:")
+print("- El rey se mueve a una casilla adyacente (horizontal, vertical o diagonal).")
+print("- La reina puede moverse en cualquier dirección (horizontal, vertical o diagonal) en cualquier número de casillas.")
+print("- Las torres se mueven horizontal o verticalmente en cualquier número de casillas.")
+print("- Los caballos se mueven en forma de 'L' (dos casillas en una dirección y luego una casilla perpendicular a la primera).")
+print("- Los alfiles se mueven diagonalmente en cualquier número de casillas.")
+print("- Los peones se mueven hacia adelante una casilla, excepto en su primer movimiento donde pueden avanzar dos casillas. Capturan en diagonal.")
+
+print("\nCaptura de piezas:")
+print("- Una pieza captura a otra si se mueve a la casilla que ocupa la pieza enemiga.")
+print("- El rey no puede moverse a una casilla que esté amenazada por una pieza enemiga.")
+print("- El jaque es cuando el rey está amenazado por una pieza enemiga.")
+print("- El jaque mate es cuando el rey está en jaque y no hay movimientos legales para escapar.")
+
+print("\nPromoción de peones:")
+print("- Si un peón alcanza la última fila del tablero, se puede promover a una dama, torre, alfil o caballo.")
+
+print("\nEnroque:")
+print("- Es un movimiento especial en el que el rey se mueve dos casillas hacia una de sus torres y la torre se coloca en la casilla adyacente al rey.")
