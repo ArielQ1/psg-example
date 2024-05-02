11361d82a20ed52eccf98c7a9b3627c64db0c547
ArielQ1 <quizayaariel@gmail.com>
2024-05-02 18:55:33 -0400

    adicion de ejercicio_01 sesion 5

---
+++ retos_sesion_05/ejercicio_01.py
@@ -0,0 +1,5 @@
+#Escribe un programa en Python que calcule el salario si la cantidad de horas trabajadas es 160 y la tarifa por hora de trabajo es 5.5 USD/hora
+horas_trabajadas = 160
+tarifa_por_hora = 5.5
+salario = horas_trabajadas * tarifa_por_hora
+print("El salario es: ", salario)
