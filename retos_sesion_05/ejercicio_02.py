c34fa387874bbe1568c1168188cdb90014273981
ArielQ1 <quizayaariel@gmail.com>
2024-05-02 19:04:50 -0400

    adicion de ejercicio_02 sesion 5

---
+++ retos_sesion_05/ejercicio_02.py
@@ -0,0 +1,11 @@
+#Escribe un programa en Python que convierta las siguientes temperaturas de grados Celsius a grados Fahrenheit:
+primero = 30 
+segundo = -273.99 
+tercero = 100 
+
+primero = (primero * 9/5) + 32 
+segundo = (segundo * 9/5) + 32
+tercero = (tercero * 9/5) + 32
+print(f"30 ºC = {primero} ºF")
+print(f"-273.99 ºC = {segundo} ºF")
+print(f"100 ºC = {tercero} ºF")
