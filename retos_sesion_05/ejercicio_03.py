1561e118a05c9d599c72b85225c924d0341a2980
ArielQ1 <quizayaariel@gmail.com>
2024-05-02 19:16:14 -0400

    adicion de ejercicio_03 sesion 5

---
+++ retos_sesion_05/ejercicio_03.py
@@ -0,0 +1,6 @@
+#Convertir a cuantos días, horas, minutos y segundos corresponde la siguiente  cantidad de segundos: 288325
+segundos = 288325
+minutos = segundos//60
+horas = minutos//60
+dias = horas//24
+print(f"{segundos} segundos son {minutos} minutos; {horas} horas y {dias} días")
