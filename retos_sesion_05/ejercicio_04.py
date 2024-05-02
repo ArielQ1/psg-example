7a8b60345d5d35d58166ad8902acc65d7124806b
ArielQ1 <quizayaariel@gmail.com>
2024-05-02 19:20:13 -0400

    adicion de ejercicio_04 sesion 5

---
+++ retos_sesion_05/ejercicio_04.py
@@ -0,0 +1,4 @@
+#Calcular el volumen de un cubo donde uno de sus lados mide 10 metros, utilizando la fórmula general del volumen de un cubo
+lado = 10
+volumen = lado**3
+print("El volumen del cubo es: ", volumen)
