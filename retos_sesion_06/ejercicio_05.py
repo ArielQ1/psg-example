7f1d5a4657ba7619f93fbeb700e4561aa2948c3a
ArielQ1 <quizayaariel@gmail.com>
2024-05-18 17:18:29 -0400

    adicion de ejercicio_05 sesion 6

---
+++ retos_sesion_06/ejercicio_05.py
@@ -0,0 +1,7 @@
+print("Comparar los números 123 y 890, comprobar si tienen la misma paridad ambos pares o ambos impares")
+numero1 = 123 #impar
+numero2 = 890 #par
+
+print("Ambos pares? ", numero1 % 2 == 0 and numero2 % 2 == 0)
+print("Ambos impares", numero1 %2 != 0 and numero2 %2 != 0)
+
