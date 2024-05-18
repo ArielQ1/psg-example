e51069bc0037b93da01eae8ccbd300158aa99825
ArielQ1 <quizayaariel@gmail.com>
2024-05-18 18:48:29 -0400

    adicion de ejercicio_02 sesion 7

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,4 @@
+print("Escribe un programa que reciba dos enteros por teclado y sume su resultado")
+num1 = int(input("Escribe un numero: "))
+num2 = int(input("Escribe otro numero: "))
+print("La suma de los dos numeros es: ", num1 + num2)
