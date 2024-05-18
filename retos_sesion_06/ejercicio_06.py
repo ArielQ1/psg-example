f7d470460908405f8b301f46f71881dff05ec5d6
ArielQ1 <quizayaariel@gmail.com>
2024-05-18 17:24:03 -0400

    adicion de ejercicio_06 sesion 6

---
+++ retos_sesion_06/ejercicio_06.py
@@ -0,0 +1,4 @@
+print("¿El cuadrado de 123 se encuentra en el siguiente rango? [N > 0 & N < 20000]")
+numero = 123
+print(f"¿El cuadrado de {numero} se encuentra en el rango?, numero: {numero**2}; {(numero**2) > 0 and (numero**2) < 20000}")
+
