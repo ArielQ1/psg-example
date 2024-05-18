7b87b4389ff7755ac655c76d77e204a31287980d
ArielQ1 <quizayaariel@gmail.com>
2024-05-18 19:11:37 -0400

    adicion de ejercicio_06 sesion 7

---
+++ retos_sesion_07/ejercicio_06.py
@@ -0,0 +1,16 @@
+print("Agregar 5 Ejemplos con otras funciones no vistas en la sesión")
+
+cadena = "Hola"
+print(cadena.center(10, "*"))
+
+cadena = "Hola, mundo!"
+print(cadena.startswith("Hola"))
+
+cadena = "Hola, mundo!"
+print(cadena.endswith("mundo!"))
+
+cadena = "Hola, mundo!"
+print(cadena.partition(", "))
+
+cadena = "Hola, mundo! Hola, todos!"
+print(cadena.rpartition("Hola")) 
