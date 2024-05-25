ecf5b2b528107971353d9d2ff5385c5350c3b006
ArielQ1 <quizayaariel@gmail.com>
2024-05-25 15:31:07 -0400

    adicion de ejercicio_02 sesion 8

---
+++ retos_sesion_08/ejercicio_02.py
@@ -0,0 +1,12 @@
+#Crea una tupla con los siguientes elementos 1,2,3,4,5,6,7,8,9,10 y realiza:
+tupla_numeros=(1,2,3,4,5,6,7,8,9,10)
+#Imprime el primer elemento
+print(tupla_numeros[0])
+#Imprime el último elemento
+print(tupla_numeros[-1])
+#Imprime un slice del 4 al 7
+print(tupla_numeros[3:7])
+#Imprime un slice del 2 al 9 con pasos de 3
+print(tupla_numeros[1:9:3])
+#Imprime un slice del 10 al 1 con saltos de -2
+print(tupla_numeros[10:1:-2])
