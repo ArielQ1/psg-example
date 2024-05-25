c863f9b1169321b137201051c7d1871b7092e3ed
ArielQ1 <quizayaariel@gmail.com>
2024-05-25 15:42:08 -0400

    adicion de ejercicio_04 sesion 8

---
+++ retos_sesion_08/ejercicio_04.py
@@ -0,0 +1,4 @@
+#Las notas de un estudiante durante un semestre son:
+#34, 61, 80, 20, 12, 69, 32, 60, 61, 51, 90, 23, 15
+tupla_notas=(34, 61, 80, 20, 12, 69, 32, 60, 61, 51, 90, 23, 15)
+print(sum(tupla_notas)/len(tupla_notas) > 51)
