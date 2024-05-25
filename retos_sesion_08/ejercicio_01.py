f970a90b7f718c4b9d1c9f638684de45d126987e
ArielQ1 <quizayaariel@gmail.com>
2024-05-25 15:26:17 -0400

    adicion de ejercicio_01 sesion 8

---
+++ retos_sesion_08/ejercicio_01.py
@@ -0,0 +1,6 @@
+#Ingresa por teclado dos coordenadas 'x','y' y una ubicación, almacena los 3 valores en una tupla e imprime el resultado
+x = input("coordenada en x: ")
+y = input("coordenada en y: ")
+ubicacion = input("ubicacion: ")
+tupla = (x, y, ubicacion)
+print(tupla, type(tupla))
