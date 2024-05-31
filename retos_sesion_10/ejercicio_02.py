df3c964ecad8b4ea8e106591cf465397d9591b39
ArielQ1 <quizayaariel@gmail.com>
2024-05-31 08:50:47 -0400

    adicion de ejercicio_02 sesion 10

---
+++ retos_sesion_10/ejercicio_02.py
@@ -0,0 +1,5 @@
+menu_mexicano = {"Tacos", "Enchiladas", "Guacamole", "Tamales"}
+menu_italiano = {"Pizza", "Pasta", "Lasagna", "Tiramisú"}
+
+menu_fusion = menu_mexicano.union(menu_italiano)
+print("Los platos disponibles en el menu actualizado son: ", menu_fusion)
