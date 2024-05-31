9a37254e1ede31d6b1f671e5a378873afcf1000c
ArielQ1 <quizayaariel@gmail.com>
2024-05-31 15:13:48 -0400

    adicion de ejercicio_01 sesion 11

---
+++ retos_sesion_11/ejercicio_01.py
@@ -0,0 +1,9 @@
+animal = {
+    "especie": "Tigre",
+    "habitat": "Selva tropical",
+    "dieta": "Carnívoro",
+    "estado_de_salud": "Bueno",
+    "edad": 5,
+    "responsables": ["Juan Pérez", "Ana Gómez", "Luis Fernández"]
+}
+print(animal)
